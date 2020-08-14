7b9bd81425f5087dd122fe43bd29292582a3a7a0
Antony Jibinraj <q500128@europe.bmw.corp>
2020-08-14 21:05:03 +0200

    unit test

--- Complete-Python-3-Bootcamp-master/07-Errors and Exception Handling/test_cap.py
+++ Complete-Python-3-Bootcamp-master/07-Errors and Exception Handling/test_cap.py
@@ -1,21 +1,21 @@
 import unittest
-import cap
+import cap2
 
 class TestCap(unittest.TestCase):
     
     def test_one_word(self):
         text = 'python'
-        result = cap.cap_text(text)
+        result = cap2.cap_text(text)
         self.assertEqual(result, 'Python')
         
     def test_multiple_words(self):
         text = 'monty python'
-        result = cap.cap_text(text)
+        result = cap2.cap_text(text)
         self.assertEqual(result, 'Monty Python')
         
     def test_with_apostrophes(self):
         text = "monty python's flying circus"
-        result = cap.cap_text(text)
+        result = cap2.cap_text(text)
         self.assertEqual(result, "Monty Python's Flying Circus")
         
 if __name__ == '__main__':
